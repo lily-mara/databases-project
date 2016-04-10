30994f2e2dacc4a4e73d0b2eef1251fd59ff910c
Nate Mara <natemara@gmail.com>
2016-04-10 14:08:36 -0400

    Add query methods to game class

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,7 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,10 +13,83 @@ public class Game extends Model {
     public float price;
 
     public List<Category> categories() {
-        return null;
+        try {
+            List<Category> categories = new ArrayList<Category>();
+            Category cat;
+
+            PreparedStatement s = c.prepareStatement(
+                    "SELECT id, name from game_category join category on id=category_id where game_id=?"
+            );
+            s.setInt(1, id);
+
+            ResultSet rs = s.executeQuery();
+            while (rs.next()) {
+                cat = new Category();
+                cat.id = rs.getInt("id");
+                cat.name = rs.getString("name");
+
+                categories.add(cat);
+            }
+
+            return categories;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public List<Review> reviews() {
-        return null;
+        try {
+            List<Review> reviews = new ArrayList<>();
+            UserReview ur;
+            CriticReview cr;
+            User u;
+
+            PreparedStatement s = c.prepareStatement(
+                    "SELECT rating, text, real_name, profile_name, credit_card, level, phone, user.id" +
+                            "from user_review" +
+                            "join user on user.id=user_review.user_id" +
+                            "where game_id=?"
+            );
+            s.setInt(1, id);
+
+            ResultSet rs = s.executeQuery();
+            while (rs.next()) {
+                ur = new UserReview();
+                ur.rating = rs.getInt("id");
+                ur.text = rs.getString("text");
+                ur.game = this;
+
+                u = new User();
+                ur.user = u;
+                u.realName = rs.getString("real_name");
+                u.creditCard = rs.getString("credit_card");
+                u.level = rs.getInt("level");
+                u.phone = rs.getString("phone");
+                u.id = rs.getInt("user.id");
+
+                reviews.add(ur);
+            }
+
+            s = c.prepareStatement("SELECT rating, text, company, link from critic_review where game_id=?");
+            s.setInt(1, id);
+
+            rs = s.executeQuery();
+            while (rs.next()) {
+                cr = new CriticReview();
+
+                cr.game = this;
+                cr.company = rs.getString("company");
+                cr.rating = rs.getFloat("rating");
+                cr.link = rs.getString("link");
+
+                reviews.add(cr);
+            }
+
+            return reviews;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
