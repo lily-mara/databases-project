cf1e18d5032d528688696ab137c71030d61fa972
Laura Paganini <paganilp@miamioh.edu>
2016-04-10 13:29:22 -0400

    name fixes

--- src/UserReview.java
+++ src/UserReview.java
@@ -1,17 +1,9 @@
 /**
  * Created by Laura on 4/10/2016.
  */
-public class UserReview implements Review {
+public class UserReview {
     public double rating;
     public String text;
     public Game game;
     public User user;
-
-    public String getText() {
-        return text;
-    }
-
-    public Game getGame() {
-        return game;
-    }
 }
