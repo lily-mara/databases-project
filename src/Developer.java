5add216ae1cfe98608b735585606a346df6bc0d9
sands <sandstkj@miamioh.edu>
2016-04-10 14:49:36 -0400

    Committing Developer.java for first time (again).

---
+++ src/Developer.java
@@ -0,0 +1,13 @@
+package src;
+
+import java.util.List;
+
+/**
+ * Created by sands on 4/10/2016.
+ */
+public class Developer {
+    public String name, website;
+    public List<Game> games() {
+        return null;
+    }
+}
