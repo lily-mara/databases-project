cea7c9dedd38479e43489b0bf013ee48f737d67f
Daltin Loomis <loomisdf@gmail.com>
2016-04-10 12:45:04 -0400

    add code for connecting to the database

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,49 @@
 /**
  * Created by loomisdf on 4/10/2016.
  */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!!!!!!");
+        // load the sqlite-JDBC driver using the current class loader
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Connection connection = null;
+        try {
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:data.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            ResultSet rs = statement.executeQuery("SELECT * FROM GAME");
+            while (rs.next()) {
+                // read the result set
+                System.out.println("Id = " + rs.getInt("id"));
+                System.out.println("Title = " + rs.getString("name"));
+                System.out.println("Price = " + rs.getInt("price"));
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e);
+            }
+        }
     }
 }
