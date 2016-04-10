c1b51f8c1bb97f28632e893e0d70ec1c26bd6e05
Nate Mara <natemara@gmail.com>
2016-04-10 14:41:46 -0400

    Update main to use models

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,6 @@
  * Created by loomisdf on 4/10/2016.
  */
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class Main {
 
     static DatabaseFrame dbFrame;
@@ -21,33 +15,11 @@ public class Main {
             e.printStackTrace();
         }
 
-        Connection connection = null;
-        try {
-            // create a database connection
-            connection = DriverManager.getConnection("jdbc:sqlite:data.db");
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+        Game g = new Game();
+        g.id = 1;
+        g.name = "Brink";
+        g.price = (float)19.99;
 
-            ResultSet rs = statement.executeQuery("SELECT * FROM GAME");
-            while (rs.next()) {
-                // read the result set
-                System.out.println("Id = " + rs.getInt("id"));
-                System.out.println("Title = " + rs.getString("name"));
-                System.out.println("Price = " + rs.getInt("price"));
-                System.out.println();
-            }
-        } catch (SQLException e) {
-            // if the error message is "out of memory",
-            // it probably means no database file is found
-            System.err.println(e.getMessage());
-        } finally {
-            try {
-                if (connection != null)
-                    connection.close();
-            } catch (SQLException e) {
-                // connection close failed.
-                System.err.println(e);
-            }
-        }
+        System.out.println(g.reviews());
     }
 }
