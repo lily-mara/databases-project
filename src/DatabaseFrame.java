917f97cc1909c6c9e6839bccf6e28978dc0e1ff1
Daltin Loomis <loomisdf@gmail.com>
2016-04-11 16:10:34 -0400

    add tabbed view to the interface

--- src/DatabaseFrame.java
+++ src/DatabaseFrame.java
@@ -9,40 +9,72 @@ import java.awt.event.ActionListener;
  */
 public class DatabaseFrame{
 
-    JFrame frame;
-    JLabel descriptionLabel;
-    JPanel panel;
-    JButton goButton;
-    JTable table;
-    DefaultTableModel tableModel;
-    JComboBox dropdown;
+    private JFrame frame;
+    private JLabel descriptionLabel;
+    private JPanel panel;
+    private JButton goButton;
+    private JTable table;
+    private DefaultTableModel tableModel;
+    private JComboBox dropdown;
+    private JTabbedPane tabPane;
+
+    private final static int extraWindowWidth = 100;
 
     public DatabaseFrame() {
         frame = new JFrame("Database Frame");
         panel = new JPanel();
         descriptionLabel = new JLabel("Welcome to the DB");
+
+        // Various creation operations
         createDropdown();
         createButtons();
         creteTable();
+        createTabPane();
 
 
-
-        frame.setSize(500, 500);
-        //frame.setLayout(new FlowLayout());
+        // Setup JFrame
+        frame.setSize(500, 525);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBackground(Color.WHITE);
 
-        panel.add(descriptionLabel);
-        panel.add(dropdown);
-        panel.add(goButton);
-        panel.add(new JScrollPane(table));
+        // Add things to panel
+        //panel.add(descriptionLabel);
+        //panel.add(dropdown);
+        //panel.add(goButton);
+        //panel.add(new JScrollPane(table));
+        panel.add(tabPane);
+
+        // Add panel to frame
         frame.add(panel);
 
+        // Set the frame visible
         frame.setVisible(true);
     }
 
+    private void createTabPane() {
+        tabPane = new JTabbedPane();
+        JPanel card1 = new JPanel();
+
+        JPanel optionPanel = new JPanel();
+        optionPanel.add(descriptionLabel);
+        optionPanel.add(dropdown);
+        optionPanel.add(goButton);
+        optionPanel.setLayout(new FlowLayout());
+
+        card1.add(optionPanel);
+        card1.add(new JScrollPane(table));
+
+        card1.setLayout(new BoxLayout(card1, BoxLayout.Y_AXIS));
+
+        JPanel card2 = new JPanel();
+        card2.add(new JLabel("User Page"));
+
+        tabPane.addTab("Store", card1);
+        tabPane.addTab("User Page", card2);
+    }
+
     private void createDropdown() {
-        String[] options = {"Show All Games","User Page","Show All Categories"};
+        String[] options = {"Show All Games", "Show All Categories"};
         dropdown = new JComboBox(options);
     }
 
