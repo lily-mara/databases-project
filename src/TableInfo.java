de894ddcda6e1071735f5c71a632d99865f34a28
Laura Paganini <paganilp@miamioh.edu>
2016-04-10 21:04:53 -0400

    tableInfo and user classes

--- src/TableInfo.java
+++ src/TableInfo.java
@@ -4,10 +4,99 @@ import java.util.List;
  * Created by nate on 4/10/16.
  */
 public class TableInfo {
-    Object rowData[][];
-    String columns[];
+    public Object rowData[][];
+    public String columns[];
 
-    TableInfo(List<User> users) {
+    public TableInfo(List<User> users) {
+        columns[0] = "Real Name";
+        columns[1] = "Profile Name";
+        columns[2] = "Credit Card";
+        columns[3] = "Level";
+        columns[4] = "Phone";
+        columns[5] = "Id";
 
+        for(int i = 0; i < users.size(); i++){
+            rowData[i][0] = users.get(i).realName;
+            rowData[i][1] = users.get(i).profileName;
+            rowData[i][2] = users.get(i).creditCard;
+            rowData[i][3] = users.get(i).level;
+            rowData[i][4] = users.get(i).phone;
+            rowData[i][5] = users.get(i).id;
+        }
     }
+
+    public TableInfo(List<Game> games) {
+        columns[0] = "Name";
+        columns[1] = "Id";
+        columns[2] = "Price";
+
+        for(int i = 0; i < games.size(); i++){
+            rowData[i][0] = games.get(i).name;
+            rowData[i][1] = games.get(i).id;
+            rowData[i][2] = games.get(i).price;
+        }
+    }
+
+    public TableInfo(List<Category> categories) {
+        columns[0] = "Id";
+        columns[1] = "Name";
+
+        for(int i = 0; i < categories.size(); i++){
+            rowData[i][0] = categories.get(i).id;
+            rowData[i][1] = categories.get(i).name;
+        }
+    }
+
+    public TableInfo(List<Developer> developers) {
+        columns[0] = "Name";
+        columns[1] = "Website";
+
+        for(int i = 0; i < developers.size(); i++){
+            rowData[i][0] = developers.get(i).name;
+            rowData[i][1] = developers.get(i).website;
+        }
+    }
+
+    public TableInfo(List<CriticReview> criticReviews) {
+        columns[0] = "Company";
+        columns[1] = "Link";
+        columns[2] = "Rating";
+        columns[3] = "Text";
+        columns[4] = "Game Id";
+
+        for(int i = 0; i < criticReviews.size(); i++){
+            rowData[i][0] = criticReviews.get(i).company;
+            rowData[i][1] = criticReviews.get(i).link;
+            rowData[i][2] = criticReviews.get(i).rating;
+            rowData[i][3] = criticReviews.get(i).text;
+            rowData[i][4] = criticReviews.get(i).game.id;
+        }
+    }
+
+    public TableInfo(List<UserReview> userReviews) {
+        columns[0] = "Rating";
+        columns[1] = "Text";
+        columns[2] = "Game Id";
+        columns[3] = "User Id";
+
+        for(int i = 0; i < userReviews.size(); i++){
+            rowData[i][0] = userReviews.get(i).rating;
+            rowData[i][1] = userReviews.get(i).text;
+            rowData[i][2] = userReviews.get(i).game.id;
+            rowData[i][3] = userReviews.get(i).user.id;
+        }
+    }
+
+    public TableInfo(List<UserGroup> userGroups) {
+        columns[0] = "Name";
+        columns[1] = "Id";
+
+        for(int i = 0; i < userGroups.size(); i++){
+            rowData[i][0] = userGroups.get(i).name;
+            rowData[i][1] = userGroups.get(i).id;
+        }
+    }
+
+
 }
+
--- src/User.java
+++ src/User.java
@@ -1,4 +1,9 @@
+import java.sql.SQLException;
 import java.util.List;
+import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 /**
  * Created by nate on 4/10/16.
@@ -12,18 +17,128 @@ public class User extends Model {
     public int id;
 
     public List<Game> games() {
-        return null;
+        List gameList = new ArrayList<Game>();
+        Game currentGame = new Game();
+
+        try {
+            Statement statement = c.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            ResultSet rs = statement.executeQuery("SELECT Id, Name, Price FROM GAME JOIN OWNS ON Id = Game_id WHERE User_id = " + id );
+
+            while (rs.next()) {         // read the result set
+                currentGame.id = rs.getInt("id");
+                currentGame.name = rs.getString("name");
+                currentGame.price = rs.getFloat("price");
+
+                gameList.add(currentGame);
+            }
+        }catch(SQLException e){
+            System.err.println(e);
+        }
+
+        return gameList;
     }
 
     public List<User> friends() {
-        return null;
+        List friendList = new ArrayList<User>();
+        User currentUser = new User();
+
+        try {
+            Statement statement = c.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            ResultSet rs = statement.executeQuery("SELECT Real_name, Profile_name, Credit_card, Level, Phone, Id FROM FRIEND JOIN USER WHERE User1 = " + id + " OR User2 = " + id);
+
+            while (rs.next()) {         // read the result set
+                currentUser.realName = rs.getString("real_name");
+                currentUser.profileName = rs.getString("profile_name");
+                currentUser.creditCard = rs.getString("credit_card");
+                currentUser.level = rs.getInt("level");
+                currentUser.phone = rs.getString("phone");
+                currentUser.id = rs.getInt("id");
+
+                friendList.add(currentUser);
+            }
+        }catch(SQLException e){
+            System.err.println(e);
+        }
+
+        return friendList;
     }
 
     public List<UserGroup> groups() {
-        return null;
+        List groupList = new ArrayList<UserGroup>();
+        UserGroup currentGroup = new UserGroup();
+
+        try {
+            Statement statement = c.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            ResultSet rs = statement.executeQuery("SELECT Name, User_Review.id FROM FRIEND JOIN USER WHERE User1 = " + id);
+
+            while (rs.next()) {         // read the result set
+                currentGroup.name = rs.getString("name");
+                currentGroup.id = rs.getInt("id");
+
+                groupList.add(currentGroup);
+            }
+        }catch(SQLException e){
+            System.err.println(e);
+        }
+
+        return groupList;
     }
 
     public List<UserReview> reviews() {
-        return null;
+        List reviewList = new ArrayList<UserReview>();
+        UserReview currentReview = new UserReview();
+
+        try {
+            Statement statement = c.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            ResultSet rs = statement.executeQuery("SELECT Rating, Text, Game_id, User_id FROM USER_REVIEW JOIN USER ON User_id = id WHERE User_id = " + id);
+
+            while (rs.next()) {         // read the result set
+                currentReview.rating = rs.getFloat("rating");
+                currentReview.text = rs.getString("text");
+                currentReview.game.id = rs.getInt("game_id");
+                currentReview.user.id = rs.getInt("user_id");
+
+                reviewList.add(currentReview);
+            }
+        }catch(SQLException e){
+            System.err.println(e);
+        }
+
+        return reviewList;
+    }
+
+    public List<User> all(){
+        List userList = new ArrayList<User>();
+        User currentUser = new User();
+
+        try {
+            Statement statement = c.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            ResultSet rs = statement.executeQuery("SELECT * FROM USER");
+
+            while (rs.next()) {         // read the result set
+                currentUser.realName = rs.getString("real_name");
+                currentUser.profileName = rs.getString("profile_name");
+                currentUser.creditCard = rs.getString("credit_card");
+                currentUser.level = rs.getInt("level");
+                currentUser.phone = rs.getString("phone");
+                currentUser.id = rs.getInt("id");
+
+                userList.add(currentUser);
+            }
+        }catch(SQLException e){
+            System.err.println(e);
+        }
+
+        return userList;
     }
 }
